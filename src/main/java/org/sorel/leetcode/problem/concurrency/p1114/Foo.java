8f6e599ac2ac6f0a8a67254c911369214a53f605
s0rel <sorelllllllll@gmail.com>
2024-02-13 08:36:08 +0800

    feat(*): Add solution to 1114

--- src/main/java/org/sorel/leetcode/problem/concurrency/p1114/Foo.java
+++ src/main/java/org/sorel/leetcode/problem/concurrency/p1114/Foo.java
@@ -1,29 +1,43 @@
 package org.sorel.leetcode.problem.concurrency.p1114;
 
-import java.util.concurrent.CountDownLatch;
-
 public class Foo {
-    private final CountDownLatch firstDone;
-    private final CountDownLatch secondDone;
+    private volatile int flag = 1;
+    private final Object object = new Object();
 
     public Foo() {
-        firstDone = new CountDownLatch(1);
-        secondDone = new CountDownLatch(1);
+
     }
 
     public void first(Runnable printFirst) throws InterruptedException {
-        printFirst.run();
-        firstDone.countDown();
+        synchronized (object) {
+            while (flag != 1) {
+                object.wait();
+            }
+
+            printFirst.run();
+            flag = 2;
+            object.notifyAll();
+        }
     }
 
     public void second(Runnable printSecond) throws InterruptedException {
-        firstDone.await();
-        printSecond.run();
-        secondDone.countDown();
+        synchronized (object) {
+            while (flag != 2) {
+                object.wait();
+            }
+
+            printSecond.run();
+            flag = 3;
+            object.notifyAll();
+        }
     }
 
     public void third(Runnable printThird) throws InterruptedException {
-        secondDone.await();
+        synchronized (object) {
+            while (flag != 3) {
+                object.wait();
+            }
+        }
         printThird.run();
     }
 }
